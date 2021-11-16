ec5d5c680b2943b89ccb65a98c5564f35d100f27
LAKSHMANARAO ALLIPALLI <92287908+LAKSHMANARAO-A@users.noreply.github.com>
2021-11-16 10:41:15 +0530

    Add files via upload

---
+++ MY NAME ON APSCII.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, c, d, e, f, g, h, i, j, k, l;
+	printf("enter tweleve numbers");
+	scanf("%d%d%d%d%d%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k,&l);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%",a+64,b+64,c+64,d+64,e+64,f+64,g+64,h+64,i+64,j+64,k+64,l+64);
+}
